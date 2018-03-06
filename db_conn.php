c7f0ba0378c276abf53cf3908a73eb855ba9a6e3
Dhayal Ram K <dhayalram.k@gmail.com>
2018-03-06 19:44:42 +0530

    Moving Credential to config

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,7 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "voise") or die(mysqli_error());
+  $configData = json_decode(file_get_contents('config.json'),true);
+
+  $conn = new mysqli($configData['host'], $configData['root'], $configData['password'], $configData['database']) or die(mysqli_error());
 
 ?>
